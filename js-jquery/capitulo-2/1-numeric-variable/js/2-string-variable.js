0770161196d2b83728404d6ec3e480e1998b60bc
danielbaia29 <danielbaia29@gmail.com>
2018-09-08 18:25:18 -0300

    adicionando exercicio cap-02

---
+++ js-jquery/capitulo-2/1-numeric-variable/js/2-string-variable.js
@@ -0,0 +1,11 @@
+
+var userName;
+var message;
+
+userName = `Daniel`;
+message = `veja seu rating`;
+
+var elName = document.getElementById(`name`);
+elName.textContent = userName;
+var elNote = document.getElementById(`note`);
+elNote.textContent = message;
---
+++ js-jquery/capitulo-2/1-numeric-variable/js/3-string-with-quotes.js
@@ -0,0 +1,14 @@
+
+var title;
+var message;
+
+title = "Molly`s Special Offers";
+message = `<a href= \"sale.html">25% OFF</a>`;
+
+var elTitle = document.getElementById("title");
+var elNote = document.getElementById("note");
+
+elTitle.innerHTML = title;
+elNote.innerHTML = message;
+
+
